c7cd2ee7e5cd9433b08e34e18a015e49651be76e
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2022-07-10 15:52:05 +0200

    Add logo text

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,24 @@ import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
+import ThemedImage from "@theme/ThemedImage";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header className={clsx("hero ", styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <ThemedImage
+          alt="Logo and 'snackstack' text"
+          sources={{
+            light: useBaseUrl("/img/logo-text-dark.svg"),
+            dark: useBaseUrl("/img/logo-text-white.svg"),
+          }}
+        />
+
         <div className={styles.buttons}>
           <Link className="button button--secondary button--lg" to="/docs">
             Get started
@@ -26,16 +34,8 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      // title={`Notification management made easy`}
-      description=""
-    >
-      <main style={{ display: "flex", justifyContent: "center" }}>WIP</main>
-
-      {/* <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main> */}
+    <Layout>
+      <HomepageHeader />
     </Layout>
   );
 }
